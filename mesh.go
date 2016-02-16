c0f0d1dfc06a34175685c76895d688a2b9c2d226
do <dageneva@gmail.com>
2016-02-16 17:49:56 +0200

    Json for mesh

--- mesh.go
+++ mesh.go
@@ -2,18 +2,20 @@ package traytor
 
 // Vertex is a single vertex in a mesh
 type Vertex struct {
-	Normal      Vec3
-	Coordinates Vec3
-	UV          Vec3
+	Normal      Vec3 `json:"normal"`
+	Coordinates Vec3 `json:"coordinates"`
+	UV          Vec3 `json:"uv"`
 }
 
 // Triangle is a face with 3 vertices (indices in the vertex array)
 type Triangle struct {
-	Points [3]int
+	Vertices [3]int `json:"vertices"`
+	Material int    `json:"material"`
+	normal   Vec3   `json:"normal"`
 }
 
 // Mesh is a triangle mesh
 type Mesh struct {
-	Vertices []Vertex
-	Faces    []Triangle
+	Vertices []Vertex   `json:"vertices"`
+	Faces    []Triangle `json:"faces"`
 }

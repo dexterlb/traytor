de42785d27ea7b8b20d7c99bdac9221412a0377c
do <dageneva@gmail.com>
2016-05-21 14:48:43 +0300

    rpc test folder in new branch

---
+++ rpc-test/server/server.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net/rpc"
+	"os"
+)
+
+func main() {
+	client, err := rpc.Dial("tcp", "localhost:42586")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		line, _, err := in.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+		var reply bool
+		err = client.Call("Listener.GetLine", line, &reply)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+Raw  server.go
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+)
+
+type Listener int
+
+func (l *Listener) GetLine(line []byte, ack *bool) error {
+	fmt.Println(string(line))
+	return nil
+}
+
+func main() {
+	addy, err := net.ResolveTCPAddr("tcp", "0.0.0.0:42586")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inbound, err := net.ListenTCP("tcp", addy)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listener := new(Listener)
+	rpc.Register(listener)
+	rpc.Accept(inbound)
+}

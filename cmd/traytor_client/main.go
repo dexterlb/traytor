d108d703510685b3bea5501e19b6871e41b36372
Angel Angelov <hextwoa@gmail.com>
2016-05-25 21:13:34 +0300

    change default address to connect to

--- cmd/traytor_client/main.go
+++ cmd/traytor_client/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"github.com/DexterLB/traytor"
-	"github.com/DexterLB/traytor/rpc"
-	"github.com/valyala/gorpc"
 	"image/png"
 	"io/ioutil"
 	"log"
 	"os"
+
+	"github.com/DexterLB/traytor"
+	"github.com/DexterLB/traytor/rpc"
+	"github.com/valyala/gorpc"
 )
 
 func main() {
 	c := &gorpc.Client{
-		Addr: ":1234",
+		Addr: "hoth:1234",
 	}
 	c.Start()
 	rr := rpc.NewRemoteRaytracer(3)
@@ -23,7 +24,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error when sending request to server: %s", err)
 	}
-	resp, err := d.Call("Sample", [2]int{800, 600})
+	resp, err := d.Call("Sample", [2]int{800, 450})
 	file, _ := os.Create("name")
 	if err != nil {
 		log.Fatalf("Error when sending request to server: %s", err)

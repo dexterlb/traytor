79d281a8514fcbaffb37d03938d8f688ef66d883
do <dageneva@gmail.com>
2016-05-27 16:31:59 +0300

    Add missing file

---
+++ cmd/traytor/worker.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/DexterLB/traytor/rpc"
+	"github.com/codegangsta/cli"
+	"github.com/valyala/gorpc"
+)
+
+func runWorker(c *cli.Context) error {
+	fmt.Printf(
+		"will start worker with %d threads on this address: %s\n",
+		c.GlobalInt("max-jobs"),
+		c.String("listen-address"),
+	)
+	address := c.String("listen-address")
+	rr := rpc.NewRemoteRaytracer(3)
+
+	w := &gorpc.Server{
+		Addr:    address,
+		Handler: rr.Dispatcher.NewHandlerFunc(),
+	}
+	if err := w.Serve(); err != nil {
+		return fmt.Errorf("Cannot start rpc server: %s", err)
+	}
+	return nil
+}

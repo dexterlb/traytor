f04ab15b3a12b5a3e97b774465255106a60f1bd3
Angel Angelov <hextwoa@gmail.com>
2016-06-23 18:42:36 +0300

    try to fix travis again

--- cmd/traytor/render.go
+++ cmd/traytor/render.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
-	"github.com/DexterLB/traytor/random"
-	"github.com/DexterLB/traytor/raytracer"
-	"github.com/DexterLB/traytor/rpc"
-	"github.com/DexterLB/traytor/scene"
 	"github.com/codegangsta/cli"
 )
 
-func runRender(c *cli.Context) {
+func runRender(c *cli.Context) error {
 	scenePath, image := getArguments(c)
 	log.Printf(
 		"will render %d samples of %s to %s of size %dx%d with %d threads",
@@ -21,32 +16,35 @@ func runRender(c *cli.Context) {
 		c.Int("max-jobs"),
 	)
 
-	width, height := c.Int("width"), c.Int("height")
-	totalSamples := c.Int("total-samples")
-	sampleCounter := rpc.NewSampleCounter(totalSamples)
-	renderedImages := make(chan *hdrimage.Image)
+	/*
+		width, height := c.Int("width"), c.Int("height")
+		totalSamples := c.Int("total-samples")
+		sampleCounter := rpc.NewSampleCounter(totalSamples)
+		renderedImages := make(chan *hdrimage.Image)
 
-	randomGen := random.New(42)
+		randomGen := random.New(42)
 
-	scene, err := scene.LoadFromFile(scenePath)
-	if err != nil {
-		return fmt.Errorf("can't open scene: %s", err)
-	}
+		scene, err := scene.LoadFromFile(scenePath)
+		if err != nil {
+			return fmt.Errorf("can't open scene: %s", err)
+		}
 
-	for i := 0; i < c.Int("max-jobs"); i++ {
-		go func() {
-			raytracer := raytracer.Raytracer{
-				Scene:  scene,
-				Random: random.New(randomGen.NewSeed()),
-			}
+		for i := 0; i < c.Int("max-jobs"); i++ {
+			go func() {
+				raytracer := raytracer.Raytracer{
+					Scene:  scene,
+					Random: random.New(randomGen.NewSeed()),
+				}
 
-			image := hdrimage.NewImage(width, height)
-			image.Divisor = 0
+				image := hdrimage.New(width, height)
+				image.Divisor = 0
 
-			for {
-				// sample image and decrease counter
-			}
+				for {
+					// sample image and decrease counter
+				}
 
-		}()
-	}
+			}()
+		}
+	*/
+	return nil
 }

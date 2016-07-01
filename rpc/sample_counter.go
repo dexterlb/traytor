51b22d8445dc828f2367a23f825f3c7a5a3faa76
Angel Angelov <hextwoa@gmail.com>
2016-07-01 22:38:06 +0300

    fix sample counter bug

--- rpc/sample_counter.go
+++ rpc/sample_counter.go
@@ -1,30 +1,34 @@
 package rpc
 
-import "sync"
+import "sync/atomic"
 
 // SampleCounter is a thread-safe  decreasing counter
 type SampleCounter struct {
-	sync.Mutex
-	Counter int
+	Counter int64
 }
 
 // NewSampleCounter initializes the counter with a value
 func NewSampleCounter(value int) *SampleCounter {
-	return &SampleCounter{Counter: value + 1}
+	return &SampleCounter{Counter: int64(value)}
 }
 
 // Dec decreases the counter by value, but doesn't go below zero (if
 // value > sc.value, the counter's value will become 0). Returns the
-// new value.
+// actual amount decremented (so the return value is 0 if the counter
+// has already been 0)
 func (sc *SampleCounter) Dec(value int) int {
-	sc.Lock()
-	defer sc.Unlock()
+	difference := int64(value)
+	newValue := atomic.AddInt64(&sc.Counter, -difference)
 
-	if sc.Counter >= value {
-		sc.Counter -= value
-	} else {
-		value = sc.Counter
-		sc.Counter = 0
+	if newValue >= 0 {
+		return int(difference)
 	}
-	return value
+
+	atomic.StoreInt64(&sc.Counter, 0)
+
+	if -newValue < difference {
+		return int(difference + newValue)
+	} else {
+		return 0
+	}
 }
